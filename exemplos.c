734e60d520f7234919471effbceadccd30e83e51
Nicolas Oliveira Veiga <nicolasveiga24@gmail.com>
2025-08-14 16:31:27 -0300

    Update exemplos.c

--- exemplos.c
+++ exemplos.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-  int n;
-  printf("Digite um numero: ");
-  scanf("%i", &n);
-  int fat = 1;
-  while (n > 0) {
-    fat *= n;
-    n--;
-  }
-  printf("Fatorial: %i\n", fat);
+  //int n;
+  //printf("Digite um numero: ");
+  //scanf("%i", &n);
+  //int fat = 1;
+  
+  //while (n > 0) {
+  //  fat *= n;
+  // n--;
+  //}
+  
+  //printf("Fatorial: %i\n", fat);
+  //int n;
+  //printf("Digite um numero: ");
+  //scanf("%i", &n);
+  //int fat = 1;
+  //for (int i = n; i > 0; i--) {
+  //  fat *= n;
+  //  n--;
+  //}
+  //printf("Fatorial: %i\n", fat);
 }
+
